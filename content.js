133f72a0aeb8867f032e916a4f77779c48e49b18
levg34 <levg34@users.noreply.github.com>
2017-10-17 21:59:13 +0200

    fisrt working version

--- content.js
+++ content.js
@@ -2,6 +2,14 @@ $('a[href]').each(function(e) {
 	var uri = $(this).attr('href')
 	if (uri.split('?u=')[1]&&uri.split('?u=')[1].split('&h=')[0]) {
 		var extracted = decodeURIComponent(uri.split('?u=')[1].split('&h=')[0])
-		console.log(extracted)
+		$(this).attr('href',extracted)
+		$(this).mousedown(function(event){
+			if (event.which === 1) {
+				alert('Right click ans select "open in new inPrivate window", or "copy url", open a new window in private and paste it. This link directs to: '+extracted)
+				//chrome.windows.create({"url": extracted, "incognito": true})
+			}
+		})
 	}
 })
+
+$('a')
