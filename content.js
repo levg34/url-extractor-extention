d5808e67c5294d422ad67b7e10fc9b6d79f67f0e
levg34 <guilhem.dross@laposte.net>
2017-10-17 16:22:51 +0200

    select only valid links

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
 $('a[href]').each(function(e) {
-	console.log($(this).attr('href'))
+	var uri = $(this).attr('href')
+	if (uri.split('?u=')[1]&&uri.split('?u=')[1].split('&h=')[0]) {
+		console.log(uri)
+	}
 })
