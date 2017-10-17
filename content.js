b572bbc61b2e2a25640e6d5c57cfdc8775a4df92
levg34 <guilhem.dross@laposte.net>
2017-10-17 16:29:17 +0200

    extract url

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 $('a[href]').each(function(e) {
 	var uri = $(this).attr('href')
 	if (uri.split('?u=')[1]&&uri.split('?u=')[1].split('&h=')[0]) {
-		console.log(uri)
+		var extracted = decodeURIComponent(uri.split('?u=')[1].split('&h=')[0])
+		console.log(extracted)
 	}
 })
