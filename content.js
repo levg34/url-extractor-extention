524930b6e8e503252773d0d072d87d453159cc47
levg34 <guilhem.dross@laposte.net>
2017-10-18 11:31:49 +0200

    fully functionnal V1

--- content.js
+++ content.js
@@ -1,15 +1,30 @@
-$('a[href]').each(function(e) {
-	var uri = $(this).attr('href')
-	if (uri.split('?u=')[1]&&uri.split('?u=')[1].split('&h=')[0]) {
-		var extracted = decodeURIComponent(uri.split('?u=')[1].split('&h=')[0])
-		$(this).attr('href',extracted)
-		$(this).mousedown(function(event){
-			if (event.which === 1) {
-				alert('Right click ans select "open in new inPrivate window", or "copy url", open a new window in private and paste it. This link directs to: '+extracted)
-				//chrome.windows.create({"url": extracted, "incognito": true})
-			}
-		})
-	}
+var links = 0
+
+chrome.storage.sync.get({
+	simple: true
+}, function (items) {
+	extract(items.simple)
 })
 
-$('a')
+function extract(simple) {
+	$('a[href]').each(function(e) {
+		var uri = $(this).attr('href')
+		if (uri.split('?u=')[1]&&uri.split('?u=')[1].split('&h=')[0]) {
+			var extracted = decodeURIComponent(uri.split('?u=')[1].split('&h=')[0])
+			$(this).attr('href',extracted)
+			if (!simple) {
+				$(this).mousedown(function(event){
+					if (event.which === 1) {
+						alert('Right click and select "open in new inPrivate window", or "copy url", open a new window in private and paste it. This link directs to: '+extracted)
+						//chrome.windows.create({"url": extracted, "incognito": true})
+					}
+				})
+			}
+			++links
+		}
+	})
+
+	chrome.storage.sync.set({'links': links}, function() {
+		console.log('Saved: '+links+' links extracted')
+	})
+}
--- options.js
+++ options.js
@@ -3,7 +3,9 @@ function save_options() {
 	var tpl = document.getElementById('tpl').value
 
 	chrome.storage.sync.set({
-		tpl: tpl
+		tpl: tpl,
+		devmode: $('#devmode').is(':checked'),
+		simple: $('#simple').is(':checked')
 	}, function () {
 		// Update status to let user know options were saved.
 		var status = document.getElementById('status');
@@ -16,10 +18,24 @@ function save_options() {
 
 function restore_options() {
 	chrome.storage.sync.get({
-		tpl: 'test/test.html'
+		tpl: 'test/test.html',
+		devmode: false,
+		simple: true
 	}, function (items) {
 		document.getElementById('tpl').value = items.tpl
+		$('#devmode').prop('checked', items.devmode)
+		if (items.devmode) {
+			$('#tp').show()
+		}
+		$('#simple').prop('checked', items.simple)
 	})
 }
 document.addEventListener('DOMContentLoaded', restore_options)
 document.getElementById('save').addEventListener('click', save_options)
+document.getElementById('devmode').addEventListener('click', function(){
+	if ($('#devmode').is(':checked')) {
+		$('#tp').show()
+	} else {
+		$('#tp').hide()
+	}
+})
